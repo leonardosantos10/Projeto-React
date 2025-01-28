e58f8caa23ff29b81fd7e9538578370e3eb6740d
Leonardo <leob35230@gmail.com>
2025-01-28 13:53:02 -0300

    alterando o código

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import styles from './App.module.css'
-import Index from "./pages"
+import Index from './pages/Index'
 
 function App() {
     return (
      <div className={styles.appContainer}>
         <Index />
-      
      </div>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,58 @@
+import { useState} from 'react';
 import TextInput from "../components/TextInput";
+import styles from "./Index.module.css";
+
 
 export default function Index() {
+const [text, setText] = useState('');
+const [tweetList, setTweetList] = useState([])
+const maxLength = 125;
+
+function onTextChange(event) {
+    const text = event.target.value;
+    if (text.length <= maxLength) {
+     setText(text);
+   }
+ }
+  
+  function sendTweet() {
+    setTweetList([...tweetList, text]);
+  }
+
+
   return (
    <div>
-     <h1>TreinaTweet</h1>
-      <div>
-        <img src={'https://github.com/leonardosantos10.png'}  />
-         <TextInput />
+     <h1 className={styles.pageTitle}>TreinaTweet</h1>
+      <div className={styles.tweetContainer}>
+        <img 
+        src={'https://github.com/leonardosantos10.png'}  
+           className={styles.avatar}
+        />
+         <TextInput 
+           placeholder={'o que está acontecendo?'}
+           rows={3}
+           maxLength={maxLength}
+           value={text}
+           onChange={onTextChange}
+         />
       </div>
-      <div>
-         <div>0 / 125</div>
-         <button>Tweetar</button>
+      <div className={styles.buttonContainer}>
+         <div>{text.length} / {maxLength}</div>
+         <button 
+               onClick={sendTweet}
+               className={styles.postButton}
+               >
+               Tweetar
+         </button>
       </div>
 
       <ul>
-        <li>Tweet 1</li>
-        <li>Tweet 2</li>
+         {tweetList.map(tweet  => {
+           // eslint-disable-next-line react/jsx-key 
+         return <li>{tweet}</li>  
+      })}
       </ul>
-
    </div>
   )
 }
+
